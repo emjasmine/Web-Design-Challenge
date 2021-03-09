5498d7e133b2a7d7f2a3b90e3ec4d29d497d0582
emjasmine <75378436+emjasmine@users.noreply.github.com>
2021-03-08 20:13:19 -0500

    update code

---
+++ .ipynb_checkpoints/csv-checkpoint.py
@@ -0,0 +1,9 @@
+# Dependencies and Setup
+import pandas as pd
+
+# File to Load (Remember to Change These)
+file_to_load = "Resources/cities.csv"
+
+# Read Purchasing File and store into Pandas data frame
+cities_data = pd.read_csv(file_to_load)
+
